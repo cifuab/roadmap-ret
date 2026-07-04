4c52238b5c0d79b0eb22945a6dc2ceb4a28deca0
cruzballDev <cruzballDev@gmail.com>
2026-07-04 21:35:46 +0200

    #02 - javascript - extra

--- Roadmap/02 - FUNCIONES Y ALCANCE/javascript/tempCodeRunnerFile.js
+++ Roadmap/02 - FUNCIONES Y ALCANCE/javascript/tempCodeRunnerFile.js
@@ -1,1 +1,17 @@
-!
+function imprimirMultiplos(texto1, texto2) {
+    let contador = 0
+    for(let i = 1; i <= 100; i++ ) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            console.log(texto1 + texto2)
+        }else if(i % 3 === 0) {
+            console.log(texto1)
+        }else if (i % 5 === 0) {
+            console.log(texto2)
+        }else {
+            console.log(i)
+            contador++
+        }
+    }
+    return contador
+}
+console.log(imprimirMultiplos("texto1", "texto2"))
